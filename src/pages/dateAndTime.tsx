b03a6b0c0f320b84a7f01b8f7679d7c99f36449d
nk0905 <0dd2n.a0905@gmail.com>
2022-02-16 10:38:37 +0900

    eslint、prettier導入

---
+++ src/pages/dateAndTime.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { convertToString } from '../services/utils';
+
+const dateAndTime: React.FC = () => {
+  const today = convertToString(new Date());
+  return (
+    <div>
+      <p>今日の日付と今の時間</p>
+      <p>
+        {`${today.year}年${today.month}月${today.date}日(${today.dayname}) 
+                ${today.hours}時${today.minutes}分${today.seconds}秒`}
+      </p>
+    </div>
+  );
+};
+export default dateAndTime;
