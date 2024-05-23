35d500cd1db9c7b4cf2dd5f67a5cb7f7242b1f71
nk0905 <0dd2n.a0905@gmail.com>
2024-05-23 20:42:51 +0900

    deepcopy関数作成

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -25,3 +25,12 @@ export const convertToString = (d: Date) => {
     milliSeconds,
   };
 };
+
+/**
+ * ディープコピーをする
+ * @param copyState コピーしたいstate
+ * @returns ディープコピーしたstateのデータ
+ */
+export const deepCopy = <T>(copyState: T): T => {
+  return JSON.parse(JSON.stringify(copyState));
+};
